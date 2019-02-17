13a605a2e1169c27eb5e1d4b0f8793c10ae85e13
daria-grebneva <greda2598@gmail.com>
2019-02-18 00:54:57 +0300

    Убрала лишнюю букву в слове weather

---
+++ observer/WeatherStationDuo/WeatherData.h
@@ -0,0 +1,152 @@
+#pragma once
+#include "Observer.h"
+#include <algorithm>
+#include <climits>
+#include <iostream>
+#include <string>
+#include <vector>
+
+struct SWeatherInfo
+{
+	double temperature = 0;
+	double humidity = 0;
+	double pressure = 0;
+	std::string stationName;
+};
+
+class CDisplay : public IObserver<SWeatherInfo>
+{
+private:
+	/* Метод Update сделан приватным, чтобы ограничить возможность его вызова напрямую
+	Классу CObservable он будет доступен все равно, т.к. в интерфейсе IObserver он
+	остается публичным
+	*/
+	void Update(SWeatherInfo const& data) override
+	{
+		std::cout << data.stationName << std::endl;
+		std::cout << "Current Temp " << data.temperature << std::endl;
+		std::cout << "Current Hum " << data.humidity << std::endl;
+		std::cout << "Current Pressure " << data.pressure << std::endl;
+		std::cout << "----------------" << std::endl;
+	}
+};
+
+class CStats
+{
+
+public:
+	void Update(const double& value)
+	{
+		if (m_min > value)
+		{
+			m_min = value;
+		}
+		if (m_max < value)
+		{
+			m_max = value;
+		}
+		m_acc += value;
+		++m_countAcc;
+		m_average = m_acc / m_countAcc;
+	}
+	void Display()
+	{
+		std::cout << "	max : " << m_max << std::endl;
+		std::cout << "	min : " << m_min << std::endl;
+		std::cout << "	average : " << m_average << std::endl;
+	}
+
+private:
+	double m_min = std::numeric_limits<double>::infinity();
+	double m_max = -std::numeric_limits<double>::infinity();
+	double m_acc = 0;
+	double m_average = 0;
+	unsigned m_countAcc = 0;
+};
+
+class CStatsDisplay : public IObserver<SWeatherInfo>
+{
+private:
+	/* Метод Update сделан приватным, чтобы ограничить возможность его вызова напрямую
+	Классу CObservable он будет доступен все равно, т.к. в интерфейсе IObserver он
+	остается публичным
+	*/
+	void Update(SWeatherInfo const& data) override
+	{
+		temperatureData.Update(data.temperature);
+		humidifyData.Update(data.humidity);
+		pressureData.Update(data.pressure);
+
+		std::cout << data.stationName << std::endl;
+		std::cout << "Temperature: " << std::endl;
+		temperatureData.Display();
+		std::cout << "Humidify: " << std::endl;
+		humidifyData.Display();
+		std::cout << "Pressure: " << std::endl;
+		pressureData.Display();
+	}
+	CStats temperatureData;
+	CStats humidifyData;
+	CStats pressureData;
+};
+
+class CWeatherData : public CObservable<SWeatherInfo>
+{
+public:
+	// Температура в градусах Цельсия
+	double GetTemperature() const
+	{
+		return m_temperature;
+	}
+	// Относительная влажность (0...100)
+	double GetHumidity() const
+	{
+		return m_humidity;
+	}
+	// Атмосферное давление (в мм.рт.ст)
+	double GetPressure() const
+	{
+		return m_pressure;
+	}
+
+	void MeasurementsChanged()
+	{
+		NotifyObservers();
+	}
+
+	void SetMeasurements(double temp, double humidity, double pressure)
+	{
+		m_humidity = humidity;
+		m_temperature = temp;
+		m_pressure = pressure;
+
+		MeasurementsChanged();
+	}
+
+	void SetStationName(std::string const& name)
+	{
+		m_stationName = name;
+	}
+
+	std::string GetStationName() const
+	{
+		return m_stationName;
+	}
+
+protected:
+	SWeatherInfo GetChangedData() const override
+	{
+		SWeatherInfo info;
+		info.temperature = GetTemperature();
+		info.humidity = GetHumidity();
+		info.pressure = GetPressure();
+		info.stationName = GetStationName();
+		return info;
+	}
+
+private:
+	double m_temperature = 0.0;
+	double m_humidity = 0.0;
+	double m_pressure = 760.0;
+	std::string m_stationName;
+};
