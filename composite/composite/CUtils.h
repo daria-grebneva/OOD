455a6b17f10dee7d63d2d9c822426dfcc6c2b455
daria-grebneva <greda2598@gmail.com>
2019-05-12 21:34:02 +0300

    Добавила тесты к GroupShape

---
+++ composite/composite/CUtils.h
@@ -0,0 +1,12 @@
+#pragma once
+#include "CShape.h"
+
+namespace Colors
+{
+auto const YELLOW = CShape::ColorToHex("ffff00ff");
+auto const BROWN = CShape::ColorToHex("4c2825ff");
+auto const DARK_BROWN = CShape::ColorToHex("331b18ff");
+auto const BLACK = CShape::ColorToHex("000000ff");
+auto const WHITE = CShape::ColorToHex("ffffffff");
+auto const LIGHT_BLUE = CShape::ColorToHex("c6e5e5ff");
+} // namespace Colors
